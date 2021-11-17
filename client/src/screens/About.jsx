d417722c83a613da461574de1c1020fd9b8b209e
rachelperez <rlb2rlp@gmail.com>
2021-11-17 00:20:43 -0800

    continue working on Carousel component..

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -1,7 +1,7 @@
 import Carousel from "../components/Carousel";
-import selfie1 from '../assets/selfie1.jpg'
-import selfie2 from '../assets/selfie2.jpg'
-import selfie3 from '../assets/selfie3.jpg'
+import selfie1 from '../assets/selfie1.jpg';
+import selfie2 from '../assets/selfie2.jpg';
+import selfie3 from '../assets/selfie3.jpg';
 
 const About = () => {
 
@@ -22,7 +22,11 @@ const About = () => {
       </a>
       </article>
       <h2>About Me</h2>
-      <Carousel imgType='selfie' imgList={images} />
+      <article id='selfie-carousel'>
+        {images.map((img, i) => (
+          <img key={i} src={img.src} alt={img.altTxt} />
+        ))}
+      </article>
       <article id="about-text">
         <p>When I'm not at my computer I like to spend my time baking, painting, and hanging out with my four dogs. (Three Mini Doxies and a Queensland Heeler mix, if you're curious!) It's always a party with loud-mouthed weenie dogs in the house.</p>
         <p>I've done tons of odd jobs throughout the last decade of my adulthood- window screen installment, dog training, and house renovation to name a few. I love using my hands and being creative in my day to day, really taking advantage of my background in fine arts to create functional but beautiful things. </p>

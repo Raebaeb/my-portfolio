4ccebc57d9a909b324f735a6a9b29cda726a89fa
rachelperez <rlb2rlp@gmail.com>
2021-11-18 11:27:29 -0800

    finish styling for mobile

--- client/src/components/ImgCrossfade.jsx
+++ client/src/components/ImgCrossfade.jsx
@@ -1,51 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ImgCrossfade extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       currImage: 0,
       arrLen: props.imgList.length,
-    }
-  };
+    };
+  }
   componentDidMount() {
     // give first array elem the show class so it renders on mount
-    const firstSlide = document.querySelector('#slideimg0');
-    firstSlide.classList.add('show')
-    this.intervalID = setInterval(() => this.nextImage(), this.props.timer)
+    const firstSlide = document.querySelector("#slideimg2");
+    firstSlide.classList.add("show");
+    // this.intervalID = setInterval(() => this.nextImage(), this.props.timer);
   }
 
   componentWillUnmount() {
-    clearInterval(this.intervalID)
+    clearInterval(this.intervalID);
   }
 
-  nextImage() {
-    const { currImage, arrLen } = this.state;
-    let elem;
-    // remove show class from current image
-    elem = document.querySelector("#slideimg" + currImage);
-    console.log(elem.classList)
-    elem.classList.remove('show');
-    // compute next image
-    this.setState({currImage: currImage + 1});
-    if (currImage >= arrLen - 1) {
-        this.setState({currImage: 0});
-    }
-    // add show class to next image
-    elem = document.querySelector("#slideimg" + currImage);
-    elem.classList.add('show');
-    console.log('current image #', currImage);
-  }
+  // nextImage() {
+  //   const { currImage, arrLen } = this.state;
+  //   let elem;
+  //   // remove show class from current image
+  //   elem = document.querySelector("#slideimg" + currImage);
+  //   console.log(elem.classList);
+  //   elem.classList.remove("show");
+  //   // compute next image
+  //   this.setState({ currImage: currImage + 1 });
+  //   if (currImage >= arrLen - 1) {
+  //     this.setState({ currImage: 0 });
+  //   }
+  //   // add show class to next image
+  //   elem = document.querySelector("#slideimg" + currImage);
+  //   elem.classList.add("show");
+  //   console.log("current image #", currImage);
+  // }
 
   render() {
     return (
-      <div className='img-container'>
+      <div className="img-container">
         {this.props.imgList?.map((img, i) => (
-          <img id={`slideimg${i}` } key={i} src={img.src} alt={img.alt} className='slide'/>
+          <img
+            id={`slideimg${i}`}
+            key={i}
+            src={img.src}
+            alt={img.alt}
+            className="slide"
+          />
         ))}
       </div>
     );
   }
-
 }
 export default ImgCrossfade;
--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ const ProjectCard = ({ project, img, className }) => {
   return (
     <div className={`project-container ${className}`}>
       <article><a href={project.url} target='_blank' rel='noopener noreferrer' className='project-title'>{project.title}</a> | <a href={project.github} target='_blank' rel='noopener noreferrer' className='project-github'>GitHub</a></article>
-      <h4>{project.desc}</h4>
+      <p>{project.desc}</p>
       <img src={img} alt={`${project.title} homepage`} />
     </div>
   );
